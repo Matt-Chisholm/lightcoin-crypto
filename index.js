968a98d5ee896313a99f2ad8dee492938db342a1
Matheson94 <mathesonchisholm@hotmail.com>
2022-01-22 16:26:01 +0000

    final push

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-let balance = 500;
-
-
 class Account {
 
-  constructor(username) {
-    this.username = username;
-    // this.balance = 0;
-    this.transactions = []
+  constructor() {
+    this.transactions = [];
   }
+
   get balance() {
-    let sum = 0;
-    this.transactions.map(value => { sum += value; })
-    return sum;
+  	let balance = 0;
+    for (let t of this.transactions) {
+    	balance += t.value;
+    }
+    return balance;
   }
-  addTransaction(value) { this.transactions.push(value); }
+
+  addTransaction(transaction) {
+  	this.transactions.push(transaction);
+  }
 }
 
+// abstract class
 class Transaction {
 
   constructor(amount, account) {
@@ -24,47 +26,72 @@ class Transaction {
   }
 
   commit() {
-    // Keep track of the time of the transaction
+    if (!this.isAllowed()) return false;
     this.time = new Date();
-    // Add the transaction to the account
-    this.account.addTransaction(this.value());
+    this.account.addTransaction(this);
     return true;
   }
+}
 
+class Withdrawal extends Transaction {
+
+  get value() {
+    return -this.amount;
+  }
+
+  isAllowed() {
+    // note how it has access to this.account b/c of parent
+    return (this.account.balance - this.amount >= 0);
+  }
 }
 
 class Deposit extends Transaction {
 
   get value() {
-    return this.amount
+    return this.amount;
   }
 
-}
-
-class Withdrawal extends Transaction {
-  value() {
-
-    return -this.amount;
-  }
-  commit() {
-    if ((this.account.balance - this.amount) < 0) {
-      return false;
-    }
-    else { super.commit() }
+  isAllowed() {
+    // deposits always allowed thanks to capitalism.
+    return true;
   }
 }
 
 
-// DRIVER CODE BELOW
+// // DRIVER CODE BELOW
 
-const myAccount = new Account('billybob');
+// const myAccount = new Account('billybob');
 
-console.log('Starting Balance:', myAccount.balance);
+// console.log('Starting Balance:', myAccount.balance);
 
-const t1 = new Deposit(120.00, myAccount);
-t1.commit();
+// const t1 = new Deposit(120.00, myAccount);
+// t1.commit();
 
-const t2 = new Withdrawal(50.00, myAccount);
-t2.commit();
+// const t2 = new Withdrawal(50.00, myAccount);
+// t2.commit();
 
-console.log('Ending Balance:', myAccount.balance);
+// console.log('Ending Balance:', myAccount.balance);
+
+// DRIVER CODE (yes, keep everything in one file for now... b/c cog load)
+const myAccount = new Account();
+
+console.log('Starting Account Balance: ', myAccount.balance);
+
+console.log('Attempting to withdraw even $1 should fail...');
+const t1 = new Withdrawal(1.00, myAccount);
+console.log('Commit result:', t1.commit());
+console.log('Account Balance: ', myAccount.balance);
+
+console.log('Depositing should succeed...');
+const t2 = new Deposit(9.99, myAccount);
+console.log('Commit result:', t2.commit());
+console.log('Account Balance: ', myAccount.balance);
+
+console.log('Withdrawal for 9.99 should be allowed...');
+const t3 = new Withdrawal(9.99, myAccount);
+console.log('Commit result:', t3.commit());
+
+console.log('Ending Account Balance: ', myAccount.balance);
+console.log("Lookings like I'm broke again");
+
+console.log('Account Transaction History: ', myAccount.transactions);
